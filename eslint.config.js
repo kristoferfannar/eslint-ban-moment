93aaac7576d69842d7566149f9c99f600f137ad4
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-23 17:56:24 +0000

    eslint config for testing purposes

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// IMPORTANT:
+// For some reason, it's necessary to include the .js suffix to this import.
+// Otherwise, an `Error [ERR_MODULE_NOT_FOUND]` occurs.
+import banMoment from "./eslint-ban-moment.js";
+
+export default {
+  files: ["**/*.js"],
+  languageOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  plugins: { kristo: banMoment },
+  rules: {
+    "kristo/eslint-ban-moment": "error",
+  },
+};
