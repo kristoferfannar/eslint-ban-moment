4fc20c1b87a9f65370441abc297d1f738b71c110
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-03-24 22:47:33 +0000

    use commonjs instead of esm
    .eslintrc.* config files seem to more or less require commonjs
    
    and using eslint.config.js with next using `next lint --config eslint.config.js` does not seem to work.

--- eslint-ban-moment.js
+++ eslint-ban-moment.js
@@ -1,6 +1,4 @@
-// IMPORTANT:
-// For some reason, it's necessary to include the .js suffix to this import.
-// Otherwise, an `Error [ERR_MODULE_NOT_FOUND]` occurs.
-import banMoment from "./ban-moment.js";
-const plugin = { rules: { "eslint-plugin-ban-moment": banMoment } };
-export default plugin;
+const banMoment = require("./ban-moment");
+const plugin = { rules: { "ban-moment": banMoment } };
+
+module.exports = plugin;
--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-// IMPORTANT:
-// For some reason, it's necessary to include the .js suffix to this import.
-// Otherwise, an `Error [ERR_MODULE_NOT_FOUND]` occurs.
-import banMoment from "./eslint-ban-moment.js";
+"use strict";
+const banMoment = require("./eslint-ban-moment");
 
-export default {
-  files: ["**/*.js"],
-  languageOptions: {
-    sourceType: "module",
-    ecmaVersion: "latest",
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+    plugins: { "ban-moment": banMoment },
+    rules: {
+      "ban-moment/ban-moment": "error",
+    },
   },
-  plugins: { kristo: banMoment },
-  rules: {
-    "kristo/eslint-plugin-ban-moment": "error",
-  },
-};
+];
